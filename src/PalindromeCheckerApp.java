43266d6ba4ada3aa288d2a31f5b7ed910eb3fb96
varshithsunkara <varshithsunkara555@gmail.com>
2026-02-27 14:57:10 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,43 +1,29 @@
-/**
- * ============================================================
- * MAIN CLASS - UseCase1PalindromeApp
- * ============================================================
- *
- * Use Case 1: Application Entry & Welcome Message
- *
- * Description:
- * This class represents the entry point of the
- * Palindrome Checker Management System.
- *
- * At this stage, the application:
- * - Starts execution from the main() method
- * - Displays a welcome message
- * - Shows application version
- *
- * No palindrome logic is implemented yet.
- *
- * The goal is to establish a clear startup flow.
- *
- * @author Developer
- * @version 1.0
- */
+public class PalindromeCheckerApp {
 
-class UseCase1PalindromeCheckerApp {
 
-    /**
-     * Application entry point.
-     *
-     * This is the first method executed by the JVM
-     * when the program starts.
-     *
-     * @param args Command-line arguments
-     */
     public static void main(String[] args) {
 
-        System.out.println("==============================================");
-        System.out.println("   Welcome to Palindrome Checker System");
-        System.out.println("==============================================");
-        System.out.println("Version: 1.0");
-        System.out.println("Application started successfully.");
+        // Hardcoded string
+        String input = "madam";
+
+        boolean isPalindrome = true;
+
+        // Loop only till half of the string length
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Display result
+        System.out.println("Input String: " + input);
+
+        if (isPalindrome) {
+            System.out.println("Result: The string is a palindrome.");
+        } else {
+            System.out.println("Result: The string is NOT a palindrome.");
+        }
     }
 }
